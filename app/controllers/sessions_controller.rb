a8e7ffbf2e509578eb9ac1bd7361067752ee0c51
Chris Tram <chris.tram@gmail.com>
2015-03-18 11:16:58 -0700

    Add helper methods for checking whether a user is logged in and returning the user object of the current user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # TODO: check that the user trying to log in is an existing user in the db. If not, show error message. If so, create a new session ID.
   end
 
   def destroy
