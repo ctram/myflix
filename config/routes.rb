a1721ba47f2091017c357b51d29ad6b5b9a854eb
Chris Tram <chris.tram@gmail.com>
2015-03-18 10:41:47 -0700

    Work on Sign-in page.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Myflix::Application.routes.draw do
   get '/' => 'videos#index'
   get '/front' => 'static_pages#front'
   get '/register' => 'users#new', as: 'register'
+  get '/sign-in' => 'sessions#new', as: 'sign-in'
 
   resources :users do
   end
+
+  resources :sessions, only: [:create]
 
   resources :videos do
     collection do
