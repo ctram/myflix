5dd6a1a8d27cfa65a27e88d44647c77990864e71
Chris Tram <chris.tram@gmail.com>
2015-03-26 14:12:14 -0700

    Remove binding.prys

--- app/controllers/my_queues_controller.rb
+++ app/controllers/my_queues_controller.rb
@@ -6,7 +6,6 @@ class MyQueuesController < ApplicationController
     if @user.my_queue == nil
       @my_queue = MyQueue.create(user_id: @user.id)
       @user.my_queue = @my_queue
-      binding.pry
     else
       @my_queue = MyQueue.find_by(user_id:@user.id)
     end
