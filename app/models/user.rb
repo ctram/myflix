3f8acf352acf73bf0126f7a3e3d57fda9e04af12
Chris Tram <chris.tram@gmail.com>
2015-04-06 17:25:53 -0700

    Add comments.'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ActiveRecord::Base
 
   has_many :reviews
   has_many :queue_items, -> { order(:position) }
-  
+
+  # A user has many relationships where he is the follower.
+  # Since User calls has_many(), the foreign key is located
+  # in the associated object (Relationship), not within User.
   has_many :following_relationships,
             class_name: 'Relationship',
             foreign_key: :follower_id
 
+  # A user has many relationships where he is the leader.
+  # Since User calls has_many(), the foreign key is located
+  # in the associated object (Relationship), not within User.
   has_many :leading_relationships,
             class_name: 'Relationship',
             foreign_key: :leader_id
